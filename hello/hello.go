b50ceff687e926d190faddef0fc8dabae5c921b5
Md Khalilul Rahman <105421454+Ashif2409@users.noreply.github.com>
2025-02-18 06:19:04 +0000

    return Greeting from multipe people

--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,9 @@ func main() {
 	log.SetPrefix("greetings: ") //before every logs it will print
 	// log.SetFlags(0) // if 0 thats mean no time and date
 
-	message, err := greetings.Hello("Ashif")
+	// message, err := greetings.Hello("Ashif")
+	names := []string{"Ashif", "Mohammed", "Khalilul"}
+	message, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
